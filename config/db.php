f55d441da7fd8ccd770ed5e572741697e1c61f7a
Igor Ostapchuk <iostapchuk@chegg.com>
2018-01-27 15:14:27 +0200

    removed string from db connection

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=' . getenv('DB_HOST') . ';dbname=' . getenv('DB_NAME') . ';unix_socket=/Applications/MAMP/tmp/mysql/mysql.sock',
+    'dsn' => 'mysql:host=' . getenv('DB_HOST') . ';dbname=' . getenv('DB_NAME'),
     'username' => getenv('DB_USER'),
     'password' => getenv('DB_PASS'),
     'charset' => 'utf8',
