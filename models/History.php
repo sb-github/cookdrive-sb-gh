44c5c8eb75495fc99a031dc0875e788f0d79f4ff
Igor Ostapchuk <iostapchuk@chegg.com>
2018-01-27 15:16:53 +0200

    removed old merge conflict string from model History

--- models/History.php
+++ models/History.php
@@ -1,15 +1,9 @@
 <?php
 /**
  * Created by PhpStorm.
- * <<<<<<< HEAD
- * User: zlemore
- * Date: 24.01.17
- * Time: 15:24
- * =======
  * User: MyBe
  * Date: 20.01.2017
  * Time: 0:27
- * >>>>>>> Igor
  */
 
 namespace app\models;
